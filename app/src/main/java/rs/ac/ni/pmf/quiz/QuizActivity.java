540f61e4aabac3cead616f062322c6c64486c0fe
perin037 <stefan.radovanovic@pmf.edu.rs>
2022-12-12 11:11:07 +0100

    Initial commit

---
+++ app/src/main/java/rs/ac/ni/pmf/quiz/QuizActivity.java
@@ -0,0 +1,14 @@
+package rs.ac.ni.pmf.quiz;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class QuizActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+    }
+}
---
+++ app/src/main/java/rs/ac/ni/pmf/quiz/StartScreenActivity.java
@@ -0,0 +1,14 @@
+package rs.ac.ni.pmf.quiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StartScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_screen);
+    }
+}
