573483f162ed9c02024d60e42733af0e49a00e6c
perin037 <stefan.radovanovic@pmf.edu.rs>
2022-12-12 13:03:05 +0100

    turn over 2 activity

--- app/src/main/java/rs/ac/ni/pmf/quiz/QuizActivity.java
+++ app/src/main/java/rs/ac/ni/pmf/quiz/QuizActivity.java
@@ -1,10 +1,38 @@
 package rs.ac.ni.pmf.quiz;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class QuizActivity extends AppCompatActivity {
+
+    public static final String KEY_USERNAME = "USERNAME";
+    public static final String KEY_SCORE = "SCORE";
+
+    public static final ActivityResultContract<String, Integer> QUIZ_CONTRACT  = new ActivityResultContract<String, Integer>() {
+        @NonNull
+        @Override
+        public Intent createIntent(@NonNull Context context, String input) {
+            final Intent intent = new Intent(context, QuizActivity.class);
+            intent.putExtra(KEY_USERNAME, input);
+            return intent;
+        }
+
+        @Override
+        public Integer parseResult(int i, @Nullable Intent intent) {
+            if(intent == null){
+                return  -1;
+            }else{
+                intent.getIntExtra(KEY_SCORE, 0);
+            }
+            return null;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
